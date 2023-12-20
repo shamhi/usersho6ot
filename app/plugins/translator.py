0add323b9ad2c125715abf0b2b61f2351387d090
ShamHi <shomakakdela@gmail.com>
2023-12-20 18:53:10 +0300

    Add reply text handler

--- app/plugins/translator.py
+++ app/plugins/translator.py
@@ -7,20 +7,28 @@ from app.utils import fn, rload
 
 
 @Client.on_message(filters.me & filters.command(['translate', 'tr', 'p'], prefixes='.'))
-@fn.with_args("<b>This command don't work without args</b>")
 async def translate_text(client: Client, message: Message):
     await message.edit(rload())
 
     translator = Translator()
     args = fn.get_command_args(message, ['translate', 'tr', 'p'])
 
-    if len(args.split()) > 1:
+    if not args:
+        return await message.edit('<emoji id=5210952531676504517>❌</emoji><b>Your args is empty</b>')
+
+    if message.reply_to_message:
+        dest = args.split()[0]
+        text = message.reply_to_message.text
+    else:
         dest, *text = args.split()
-
         text = " ".join(text)
-        if dest not in LANGCODES.values():
-            return await message.edit(f'<emoji id=5210952531676504517>❌</emoji>{dest} is not lang code')
 
-        ttext = translator.translate(text, dest=dest).text
+    if dest not in LANGCODES.values():
+        return await message.edit(f'<emoji id=5210952531676504517>❌</emoji><b>{dest}</b> is not lang code')
 
-        return await message.edit(ttext)
+    translated = translator.translate(text, dest=dest)
+    text = translated.text
+    from_lang = translated.src
+    to_lang = translated.dest
+
+    return await message.edit(f'From <b><i>{from_lang}</i></b> to <b><i>{to_lang}</i></b>\n\n{text}')
