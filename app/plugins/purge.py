18b5f3cd99f66df3a8981ae3114d5ae4795bd13b
ShamHi <shomakakdela@gmail.com>
2024-01-03 19:45:42 +0300

    Add purge method

--- app/plugins/purge.py
+++ app/plugins/purge.py
@@ -1,6 +1,8 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
+import asyncio
+
 from app.utils import fn
 
 
@@ -10,3 +12,20 @@ async def delete_message(client: Client, message: Message):
     await message.delete()
     await message.reply_to_message.delete()
 
+
+@Client.on_message(filters.me & filters.command('purge', prefixes='.'))
+@fn.with_reply
+async def purge(client: Client, message: Message):
+    chunk = []
+    async for msg in client.get_chat_history(chat_id=message.chat.id):
+        if msg.id < message.reply_to_message.id:
+            break
+
+        chunk.append(msg.id)
+        if len(chunk) >= 100:
+            await client.delete_messages(chat_id=message.chat.id, message_ids=chunk)
+            chunk.clear()
+            await asyncio.sleep(1)
+
+    if chunk:
+        await client.delete_messages(chat_id=message.chat.id, message_ids=chunk)
