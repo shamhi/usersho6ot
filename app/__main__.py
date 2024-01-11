d8f0e21dc25d66f1d24087509cba4b81b0a0c6c0
ShamHi <shomakakdela@gmail.com>
2024-01-11 19:45:23 +0300

    Add loguru logs

--- app/__main__.py
+++ app/__main__.py
@@ -1,11 +1,24 @@
 from pyrogram import Client, compose
+
+from loguru import logger
+from sys import stderr
 import asyncio
 import logging
 
 from app.config import Config
+from app.utils.database_actions import on_startup_database
+
+
+logger.remove()
+logger.add(stderr, format='<white>{time:HH:mm:ss}</white>'
+                          ' | <level>{level: <8}</level>'
+                          ' | <cyan>{line}</cyan>'
+                          ' - <white>{message}</white>')
 
 
 async def main():
+    await on_startup_database()
+
     app1 = Client(
         name=Config.SESSION_NAME1,
         api_id=Config.API_ID1,
