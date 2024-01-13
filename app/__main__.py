e428577787b5800732a1878c6d8aff04e79ba319
ShamHi <shomakakdela@gmail.com>
2024-01-13 17:52:24 +0300

    Update __main__.py

--- app/__main__.py
+++ app/__main__.py
@@ -1,12 +1,12 @@
-from pyrogram import Client, compose
-
-from loguru import logger
-from sys import stderr
 import asyncio
 import logging
+from os.path import exists
+from sys import stderr
+
+from pyrogram import Client, compose
+from loguru import logger
 
 from app.config import Config
-from app.utils.database_actions import on_startup_database
 
 
 logger.remove()
@@ -17,25 +17,21 @@ logger.add(stderr, format='<white>{time:HH:mm:ss}</white>'
 
 
 async def main():
-    await on_startup_database()
-
     app1 = Client(
         name=Config.SESSION_NAME1,
         api_id=Config.API_ID1,
         api_hash=Config.API_HASH1,
-        plugins=dict(root='app/plugins'),
+        plugins=dict(root='plugins' if exists(path='plugins') else 'app/plugins'),
     )
 
     app2 = Client(
         name=Config.SESSION_NAME2,
         api_id=Config.API_ID2,
         api_hash=Config.API_HASH2,
-        plugins=dict(root='app/plugins'),
+        plugins=dict(root='plugins' if exists(path='plugins') else 'app/plugins'),
     )
 
     await compose([app1, app2])
-
-
 
 
 if __name__ == '__main__':
