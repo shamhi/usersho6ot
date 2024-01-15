973be5dd78a39e30676a731a6edb9b8e5b71d09f
ShamHi <shomakakdela@gmail.com>
2024-01-15 19:00:33 +0300

    Optimize imports

--- app/plugins/spaming.py
+++ app/plugins/spaming.py
@@ -1,8 +1,8 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message
-
 import asyncio
 import random
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
 
 from app.utils import fn
 
