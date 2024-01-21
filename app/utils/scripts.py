2cdbec80815617e27ec96a1b46e3ae73129e68f3
ShamHi <shomakakdela@gmail.com>
2024-01-21 11:33:13 +0300

    Update scripts

--- app/utils/scripts.py
+++ app/utils/scripts.py
@@ -1,10 +1,8 @@
-import os
 from typing import Union
 
 from pyrogram import Client
 from pyrogram.types import Message
 
-from loguru import logger
 import asyncio
 import aiohttp
 import string
@@ -163,43 +161,6 @@ async def paste_yaso(code: str, expiration_time: int = 10080):
         return "Pasting failed"
     else:
         return f"https://yaso.su/{result.get('url')}"
-
-
-def get_value_by_json(file_json: dict,
-                      *keys) -> str | None:
-    for key in keys:
-        if key in file_json:
-            return file_json[key]
-    return None
-
-
-async def read_session_json_file(session_name: str) -> dict:
-    file_path: str = f'sessions/{session_name}.json'
-    result_dict: dict = {}
-
-    try:
-        if not await exists(file_path):
-            return result_dict
-
-        async with aiofiles.open(file=file_path,
-                                 mode='r',
-                                 encoding='utf-8') as file:
-            file_json: dict = loads(await file.read())
-
-        result_dict: dict = {
-            'api_id': get_value_by_json(file_json, 'api_id', 'app_id', 'apiId', 'appId'),
-            'api_hash': get_value_by_json(file_json, 'api_hash', 'app_hash', 'apiHash', 'appHash'),
-            'device_model': get_value_by_json(file_json, 'deviceModel', 'device'),
-            'system_version': get_value_by_json(file_json, 'systemVersion', 'system_version', 'appVersion', 'app_version'),
-            'app_version': get_value_by_json(file_json, 'appVersion', 'app_version'),
-            'lang_code': get_value_by_json(file_json, 'lang_pack', 'langCode', 'lang'),
-            'system_lang_code': get_value_by_json(file_json, 'system_lang_pack', 'systemLangCode', 'systemLangPack')
-        }
-
-    except Exception as error:
-        logger.error(f'{session_name} | Ошибка при чтении .json файла: {error}')
-
-    return result_dict
 
 
 async def get_weather(city: str) -> str:
