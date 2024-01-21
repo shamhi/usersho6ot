10c5f82d627e1841fe4495abb1a4e2fc1f3bb1cf
ShamHi <shomakakdela@gmail.com>
2024-01-21 11:32:41 +0300

    Update config

--- app/config/config.py
+++ app/config/config.py
@@ -21,14 +21,3 @@ class Config:
 
     EDEN_API = getenv('EDEN_API', str)
     WEATHER_API = getenv('WEATHER_API', str)
-
-    # NotCoin
-    MIN_CLICKS_COUNT: int = 1
-    AUTO_BUY_ENERGY_BOOST: bool = False
-    AUTO_BUY_SPEED_BOOST: bool = True
-    AUTO_BUY_CLICK_BOOST: bool = True
-    USE_PROXY_FROM_FILE: bool = False
-    SLEEP_BETWEEN_CLICK: list[int] = [10, 15]
-    SLEEP_BEFORE_BUY_MERGE: list[int] = [10, 15]
-    SLEEP_BEFORE_ACTIVATE_FREE_BUFFS: list[int] = [10, 15]
-    SLEEP_BEFORE_ACTIVATE_TURBO: list[int] = [10, 15]
