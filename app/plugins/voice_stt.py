5ccdb0041ba4fd401135974fc922c1e061d6aa24
ShamHi <shomakakdela@gmail.com>
2024-01-27 13:03:42 +0300

    Update voice stt

--- app/plugins/voice_stt.py
+++ app/plugins/voice_stt.py
@@ -4,9 +4,12 @@ from pyrogram.types import Message
 from app.utils import fn
 
 
-@Client.on_message((filters.audio | filters.voice | filters.video | filters.video_note) & filters.private)
+@Client.on_message(filters.me & filters.command('stt', prefixes='.'))
+@fn.with_reply
 async def parse_text(client: Client, message: Message):
-    path = await client.download_media(message, file_name='downloads/media.mp3')
+    await message.delete()
+
+    path = await client.download_media(message.reply_to_message, file_name='downloads/media.mp3')
     file_bytes = fn.get_file_bytes_by_path(path)
 
     parsed_text = await fn.speech_to_text(file_bytes=file_bytes)
